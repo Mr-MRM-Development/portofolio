1a1fb8cf5ed424bb71c3b7a27f2759941140f398
Mohammad Rizqy Mubarok <mubarokrizqy101@gmail.com>
2025-09-13 06:40:47 +0700

    Delete drive directory

--- drive/C/system_32/app_manager/js/data.js
+++
@@ -1,9 +0,0 @@
-let jsonData;
-let appData;
-let appName;
-let appPath;
-let iconPath;
-let appNameInput = document.getElementById("appNameInput");
-let appPathInput = document.getElementById("appPathInput");
-let iconPathInput = document.getElementById("iconPathInput");
-let appBox = document.getElementById("app-box");
